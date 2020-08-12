ef87dbd9d1c7555995f5b7ffc51abbfba47df6f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-12 00:14:25 +0000

    Deploy thiagopt/demo-app to github.com/thiagopt/demo-app.git:gh-pages

---
+++ precache-manifest.d20ad2c78b694c0e8bbf4e50d8effab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2e387b1e4026729631",
+    "url": "/demo-app/static/css/main.abc75da8.chunk.css"
+  },
+  {
+    "revision": "2f2e387b1e4026729631",
+    "url": "/demo-app/static/js/main.2f2e387b.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c129fc464d02d057789076332ef4ca8f",
+    "url": "/demo-app/index.html"
+  }
+];
